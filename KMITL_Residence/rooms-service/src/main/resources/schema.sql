c850bd6cc26afe30d53ec41dfdf4f73d777e0720
s7070112@kmitl.ac.th <s7070112@kmitl.ac.th>
2017-03-07 11:49:03 +0700

    fix utf8 problem

--- KMITL_Residence/rooms-service/src/main/resources/schema.sql
+++ KMITL_Residence/rooms-service/src/main/resources/schema.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXISTS ROOMS;
-DROP TABLE IF EXISTS ROOMS_TYPE;
+DROP TABLE IF EXISTS ROOM_TYPE;
 DROP TABLE IF EXISTS ROOMSCHECKER;
 
 CREATE TABLE Room_Type
 (
     type_id int PRIMARY KEY,
-    type_name_th VARCHAR(45),
+    type_name_th VARCHAR(45) CHARACTER SET utf8 COLLATE utf8_general_ci,
     type_name_en VARCHAR(80),
-    type_details_th VARCHAR(2000),
+    type_details_th VARCHAR(2000) CHARACTER SET utf8 COLLATE utf8_general_ci,
     type_details_en VARCHAR(2000),
     type_price DOUBLE not null,
     type_adult_limit INT not null,
@@ -18,8 +18,7 @@ CREATE TABLE Rooms
 (
     room_id int PRIMARY KEY,
     room_details VARCHAR(500),
-    room_type_id int,
-    FOREIGN KEY (room_type_id) REFERENCES Room_Type(type_id)
+    room_type_id int
 );
 
 CREATE TABLE RoomsChecker
@@ -27,6 +26,5 @@ CREATE TABLE RoomsChecker
     reservation_id INT PRIMARY KEY,
     checkin DATE NOT NULL,
     checkout DATE,
-    room_id INT,
-    FOREIGN KEY (room_id) REFERENCES Rooms(room_id)
+    room_id INT
 );
