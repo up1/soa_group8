429a6dd9cfa07ac37b77248d39b042ece870f725
s7070112@kmitl.ac.th <s7070112@kmitl.ac.th>
2017-03-18 04:46:11 +0700

    explode to component

--- KMITL_Residence/front-end/src/components/Home.vue
+++ KMITL_Residence/front-end/src/components/Home.vue
@@ -21,40 +21,7 @@
           <h1 class="ui center aligned header section-header">RESERVATION</h1>
         </div>
         <div class="sixteen wide column">
-          <form class="ui form">
-            <div class="fields">
-              <div class="ui large input four wide field">
-                <label>Date Check-in</label>
-                <input type="date">
-              </div>
-              <div class="ui large input four wide field">
-                <label>Date Check-out</label>
-                <input type="date">
-              </div>
-              <div class="ui large input two wide field">
-                <label>Adults</label>
-                <select class="ui dropdown">
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                </select>
-              </div>
-              <div class="ui large input two wide field">
-                <label>Children</label>
-                <select class="ui dropdown">
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                </select>
-              </div>
-              <div class="ui large input four wide field">
-                <label>​&nbsp;</label>
-                <button class="ui button fluid luxury-primary">
-                  Book Room
-                </button>
-              </div>
-            </div>
-          </form>
+          <reservation-form/>
         </div>
       </div>
     </div>
@@ -63,8 +30,13 @@
 </template>
 
 <script>
+import ReservationForm from './ReservationForm'
+
 export default {
-    name: 'home'
+    name: 'home',
+    components: {
+      ReservationForm
+    }
 }
 </script>
 
@@ -78,12 +50,5 @@ div.hero-middle-vertical {
   vertical-align: middle;
 }
 
-div.input {
-  display: block;
-}
-
-div.large.input.field > select, div.large.input.field > button{
-  height: 43.11px;
-}
 
 </style>
---
+++ KMITL_Residence/front-end/src/components/ReservationForm.vue
@@ -0,0 +1,56 @@
+<template>
+<form class="ui form">
+    <div class="fields">
+        <div class="ui large input four wide field">
+            <label>Date Check-in</label>
+            <input type="date">
+        </div>
+
+        <div class="ui large input four wide field">
+            <label>Date Check-out</label>
+            <input type="date">
+        </div>
+
+        <div class="ui large input two wide field">
+            <label>Adults</label>
+            <select class="ui dropdown">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+            </select>
+        </div>
+
+        <div class="ui large input two wide field">
+            <label>Children</label>
+            <select class="ui dropdown">
+                <option value="1">1</option>
+                <option value="2">2</option>
+            </select>
+        </div>
+
+        <div class="ui large input four wide field">
+            <label>​&nbsp;</label>
+            <button class="ui button fluid luxury-primary">
+                Book Room
+            </button>
+        </div>
+    </div>
+</form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+div.input {
+  display: block;
+}
+
+div.large.input.field > select, div.large.input.field > button{
+  height: 43.11px;
+}
+</style>
