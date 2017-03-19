085638c887cbb6a9e6033bf6621d7a56690e9942
s7070112@kmitl.ac.th <s7070112@kmitl.ac.th>
2017-03-19 10:37:02 +0700

    add step element to reservation component

--- KMITL_Residence/front-end/src/components/Reservation.vue
+++ KMITL_Residence/front-end/src/components/Reservation.vue
@@ -2,6 +2,29 @@
     <div>
         <div class="content-container extra-top-menubar-padding">
             <div class="card-padding">
+                <div class="ui two steps">
+                    <a class="active step">
+                        <i class="truck icon"></i>
+                        <div class="content">
+                            <div class="title">Shipping</div>
+                            <div class="description">Choose your shipping options</div>
+                        </div>
+                    </a>
+                    <a class="step">
+                        <i class="payment icon"></i>
+                        <div class="content">
+                            <div class="title">Billing</div>
+                            <div class="description">Enter billing information</div>
+                        </div>
+                    </a>
+                </div>
+                
+            </div>
+                
+
+        </div>
+        <div class="content-container">
+            <div class="card-padding">
                 <div class="card padding-content">
                     <div class="ui grid">
                         <div class="sixteen wide column">
