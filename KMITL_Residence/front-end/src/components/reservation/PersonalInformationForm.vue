918dbd93b87f565a711efaf092b48abc5329fbda
s7070112@kmitl.ac.th <s7070112@kmitl.ac.th>
2017-03-29 18:47:38 +0700

    add form validator

--- KMITL_Residence/front-end/src/components/reservation/PersonalInformationForm.vue
+++ KMITL_Residence/front-end/src/components/reservation/PersonalInformationForm.vue
@@ -1,49 +1,62 @@
 <template>
-<form class="ui large form" @submit.prevent>
-    <div class="field">
-        <label>Name </label>
-        <div class="fields">
-            <div class="two wide field">
-                <select class="ui fluid search dropdown" v-model="personalInformation.titleName" value="Mr.">
-                    <option value="Mr.">Mr.</option>
-                    <option value="Mrs.">Mrs.</option>
-                    <option value="Miss">Miss</option>
-                </select>
-            </div>
-            <div class="seven wide field">
-                <input type="text" name="firstname" placeholder="Firstname" v-model="personalInformation.firstName">
-            </div>
-            <div class="seven wide field">
-                <input type="text" name="lastname" placeholder="Lastname" v-model="personalInformation.lastName">
-            </div>
-        </div>
-        <div class="two fields">
-            <div class="field">
-                <label>Nationality</label>
-                <Nationality v-model="personalInformation.nation"/>
-            </div>
-            <div class="field">
-                <label>Country</label>
-                <Country v-model="personalInformation.country"/>
-            </div>
-        </div>
-        <div class="two fields">
-            <div class="field">
-                <label>E-mail</label>
-                <input type="text" name="email" placeholder="Your e-mail" v-model="personalInformation.email">
-            </div>
-            <div class="field">
-                <label>Phone Number</label>
-                <input type="text" name="tel" placeholder="Your Phone Number" v-model="personalInformation.tel">
-            </div>
-        </div>
-        <div class="fields">
-            <div class="sixteen wide field">
-                <button class="fluid ui blue large button" id="nextbtn" @click="next">Next</button>
+<div>
+    <div class="ui grid" v-if="errors.any()">
+        <div class="sixteen wide column">
+            <div class="ui error message">
+                <div class="header">Got error!</div>
+                <ul class="list" v-for="error in errors.all()">
+                    <li>{{ error }}</li>
+                </ul>
             </div>
         </div>
     </div>
-</form>
+    <div class="ui hidden divider" v-if="errors.any()"></div>
+    <form class="ui large form" @submit.prevent>
+        <div class="field">
+            <label>Name </label>
+            <div class="fields">
+                <div class="two wide field">
+                    <select class="ui fluid search dropdown" v-model="personalInformation.titleName" value="Mr.">
+                        <option value="Mr.">Mr.</option>
+                        <option value="Mrs.">Mrs.</option>
+                        <option value="Miss">Miss</option>
+                    </select>
+                </div>
+                <div class="seven wide field" :class="{'error': errors.has('firstname')}">
+                    <input v-validate="'required|alpha'" type="text" name="firstname" placeholder="Firstname" v-model="personalInformation.firstName">
+                </div>
+                <div class="seven wide field" :class="{'error': errors.has('lastname')}">
+                    <input v-validate="'required|alpha'" type="text" name="lastname" placeholder="Lastname" v-model="personalInformation.lastName">
+                </div>
+            </div>
+            <div class="two fields">
+                <div class="field">
+                    <label>Nationality</label>
+                    <Nationality v-model="personalInformation.nation"/>
+                </div>
+                <div class="field">
+                    <label>Country</label>
+                    <Country v-model="personalInformation.country"/>
+                </div>
+            </div>
+            <div class="two fields">
+                <div :class="{'field': true, 'error':errors.has('email')}">
+                    <label>E-mail</label>
+                    <input v-validate="'required|email'" type="text" name="email" placeholder="Your e-mail" v-model="personalInformation.email">
+                </div>
+                <div class="field">
+                    <label>Phone Number</label>
+                    <input type="text" name="tel" placeholder="Your Phone Number" v-model="personalInformation.tel">
+                </div>
+            </div>
+            <div class="fields">
+                <div class="sixteen wide field">
+                    <button class="fluid ui blue large button" id="nextbtn" @click="next">Next</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
 </template>
 
 <script>
@@ -75,7 +88,9 @@ export default {
             this.personalInformation.firstName = _.capitalize(this.personalInformation.firstName)
             this.personalInformation.lastName = _.capitalize(this.personalInformation.lastName)
             this.$emit('input', this.personalInformation)
-            this.$emit('next')
+            if(!this.errors.any()){
+                this.$emit('next')
+            }
         }
     }
 
--- KMITL_Residence/front-end/src/main.js
+++ KMITL_Residence/front-end/src/main.js
@@ -13,7 +13,10 @@ import store from './store'
 import App from './App'
 import router from './router'
 
+import VeeValidate from 'vee-validate'
+
 Vue.config.productionTip = false
+Vue.use(VeeValidate)
 
 /* eslint-disable no-new */
 
