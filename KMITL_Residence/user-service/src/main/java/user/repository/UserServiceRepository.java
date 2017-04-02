6aed7b532cdea8b5bd0783fdba9f80c9e833f224
Adisorn Sripakpaisit <57070140@kmitl.ac.th>
2017-04-02 08:31:28 +0700

    implement createUser

--- KMITL_Residence/user-service/src/main/java/user/repository/UserServiceRepository.java
+++ KMITL_Residence/user-service/src/main/java/user/repository/UserServiceRepository.java
@@ -1,5 +1,6 @@
 package user.repository;
 
+import org.h2.util.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +23,23 @@ public class UserServiceRepository {
     @Transactional
     public void createUser(User user) {
 
+        String sql_insert_account = "insert into user_account (username, hash_password, role_id, status) " +
+                "values(?, ?, ?, 'Active');";
+        String sql_insert_staff = "insert into staff (th_prename, th_name, en_prename, en_name, email, username) " +
+                "values(?, ?, ?, ?, ?, ?);";
+
+        jdbc.update(sql_insert_account,
+                user.getUsername(),
+                Utils.hashCode(user.getPassword()),
+                user.getRole());
+        jdbc.update(sql_insert_staff,
+                user.getTitleNameTh(),
+                user.getFullNameTh(),
+                user.getTitleNameEn(),
+                user.getFullNameEn(),
+                user.getEmail(),
+                user.getUsername());
+
     }
 
     @Transactional
@@ -37,6 +55,21 @@ public class UserServiceRepository {
     @Transactional(readOnly = true)
     public UserInformation getUser(String username) {
         UserInformation user = null;
+
+        String sql = "select a.username, r.en_role, s.th_prename, s.th_name, s.en_prename, s.en_name, s.email " +
+                "from user_account a " +
+                "join staff s " +
+                "on a.username = s.username " +
+                "join user_role r " +
+                "on r.role_id = a.role_id" +
+                "where a.username = " + username + ";";
+
+        try {
+
+        }catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
         return user;
     }
 
