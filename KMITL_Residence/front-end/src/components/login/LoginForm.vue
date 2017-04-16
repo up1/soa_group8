19b7b1f79c599bf1258d719cd076f2efe99f8f92
Akkapon Chainarong <autz-ii@hotmail.com>
2017-04-16 21:40:06 +0700

    add login components

---
+++ KMITL_Residence/front-end/src/components/login/LoginForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <form class="ui form">
+      <div class="field">
+        <label>Username</label>
+        <input type="text" name="username" placeholder="Username or Email">
+      </div>
+      <div class="field">
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Password">
+      </div>
+      <div class="ui center aligned">
+        <button class="ui button" type="submit">Login</button>
+        <button class="ui button" type="submit">Register</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
---
+++ KMITL_Residence/front-end/src/components/login/LoginPanel.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ui raised centered card">
+  	<div class="content">
+  		<login-form/>
+  	</div>
+  </div>
+</template>
+
+<script>
+import LoginForm from './LoginForm'
+export default {
+    components: {
+        LoginForm
+    }
+}
+</script>
