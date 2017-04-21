beb511b6632f4c6f7030dce00254bce824f7d0bb
s7070112@kmitl.ac.th <s7070112@kmitl.ac.th>
2017-04-22 02:04:57 +0700

    get API URL from env variable

--- KMITL_Residence/front-end/src/services/rooms.js
+++ KMITL_Residence/front-end/src/services/rooms.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const API_URL = process.env.API_URL
+
 const getRoomTypeData = (roomTypeId) => {
-    return axios(`http://localhost:9001/rooms/type/${roomTypeId}`)
+    return axios(`http://${API_URL}:9001/rooms/type/${roomTypeId}`)
 }
 
 export default {
